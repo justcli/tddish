e1d70810b25985f7d4ef9efca0c658c75b19cfcb
Webpandey <admin@Viveks-MacBook-Pro.local>
2021-01-26 15:10:25 +0530

    "added: module support"

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tddish",
+    version="1.0.0",
+    author="justcli",
+    author_email="pandey.justcli@fastmail.com",
+    description="A tool for TDD based Test-While-Coding experience",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/justcli/ttdish",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
